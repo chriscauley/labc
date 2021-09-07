3165beae67cd4816649e715e4d6d5a1c78ace1c3
chriscauley <chris@lablackey.com>
2021-09-07 17:36:18 +0000

    yarn boilerplate

---
+++ client/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+<div>Woo!,</div>
+</template>
+
+<script>
+export default {
+}
+</script>
---
+++ client/vue.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  lintOnSave: false,
+  devServer: {
+    host: 'labc.localhost',
+    port: 8683,
+    historyApiFallback: true,
+  },
+}
